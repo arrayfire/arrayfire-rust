9b311bdd155badf98205462a6e8ee17bd49c9a7a
pradeep <pradeep@arrayfire.com>
2015-07-20 11:09:41 -0400

    Modified pi example to report benchmarked runtime

--- examples/pi.rs
+++ examples/pi.rs
@@ -5,6 +5,8 @@ use time::PreciseTime;
 use af::Dim4;
 use af::Aftype;
 
+#[allow(unused_must_use)]
+#[allow(unused_variables)]
 fn main() {
     af::set_device(0);
     af::info();
@@ -15,14 +17,17 @@ fn main() {
     let y = &af::randu(dims, Aftype::F32).unwrap();
 
     let start = PreciseTime::now();
-    let pi_val = af::add(&(x*x), &(y*y))
-    //let pi_val = af::sqrt(&(x*x) + &(y*y))
-        .and_then( |z| af::sqrt(&z) )
-        .and_then( |z| af::le(&z, &af::constant(1, dims).unwrap()) )
-        .and_then( |z| af::sum_all(&z) )
-        .map( |z| z.0 * 4.0/(samples as  f64) )
-        .unwrap();
+
+    for bench_iter in 0..100 {
+        let pi_val = af::add(&(x*x), &(y*y))
+            .and_then( |z| af::sqrt(&z) )
+            .and_then( |z| af::le(&z, &af::constant(1, dims).unwrap()) )
+            .and_then( |z| af::sum_all(&z) )
+            .map( |z| z.0 * 4.0/(samples as  f64) )
+            .unwrap();
+    }
+
     let end = PreciseTime::now();
 
-    println!("Estimated Pi Value: {} in {} seconds", pi_val, start.to(end));
+    println!("Estimated Pi Value in {} seconds", start.to(end) / 100);
 }
