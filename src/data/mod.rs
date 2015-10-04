201b31ea80c857b8eb46b907b689e472d0b86370
pradeep <pradeep@arrayfire.com>
2015-10-04 16:16:40 -0400

    Documentation for modules arith, blas, data, device & image

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -174,10 +174,33 @@ cnst!(u32 , 6);
 cnst!(u8  , 7);
 
 
+/// Create an Array with constant value
+///
+/// # Parameters
+///
+/// - `cnst` is the constant value to be filled in the Array
+/// - `dims` is the size of the constant Array
+///
+/// # Return Values
+///
+/// An Array of given dimensions with constant value
 pub fn constant<T : ConstGenerator>(cnst: T, dims: Dim4) -> Result<Array, AfError> {
     cnst.generate(dims)
 }
 
+/// Create a Range of values
+///
+/// Creates an array with [0, n] values along the `seq_dim` which is tiled across other dimensions.
+///
+/// # Parameters
+///
+/// - `dims` is the size of Array
+/// - `seq_dim` is the dimension along which range values are populated, all values along other
+/// dimensions are just repeated
+/// - `aftype` is the type of the Array.
+///
+/// # Return Values
+/// Array
 #[allow(unused_mut)]
 pub fn range(dims: Dim4, seq_dim: i32, aftype: Aftype) -> Result<Array, AfError> {
     unsafe {
@@ -192,6 +215,19 @@ pub fn range(dims: Dim4, seq_dim: i32, aftype: Aftype) -> Result<Array, AfError>
     }
 }
 
+/// Create a range of values
+///
+/// Create an sequence [0, dims.elements() - 1] and modify to specified dimensions dims and then tile it according to tile_dims.
+///
+/// # Parameters
+///
+/// - `dims` is the dimensions of the sequence to be generated
+/// - `tdims` is the number of repitions of the unit dimensions
+/// - `aftype` is the type of Array to generate
+///
+/// # Return Values
+///
+/// Array
 #[allow(unused_mut)]
 pub fn iota(dims: Dim4, tdims: Dim4, aftype: Aftype) -> Result<Array, AfError> {
     unsafe {
@@ -207,6 +243,7 @@ pub fn iota(dims: Dim4, tdims: Dim4, aftype: Aftype) -> Result<Array, AfError> {
     }
 }
 
+/// Set seed for random number generation
 pub fn set_seed(seed: u64) -> Result<(), AfError> {
     unsafe {
         let err_val = af_set_seed(seed as Uintl);
@@ -217,6 +254,7 @@ pub fn set_seed(seed: u64) -> Result<(), AfError> {
     }
 }
 
+/// Get the seed of random number generator
 #[allow(unused_mut)]
 pub fn get_seed() -> Result<u64, AfError> {
     unsafe {
@@ -251,6 +289,17 @@ data_gen_def!(randu, af_randu);
 data_gen_def!(randn, af_randn);
 data_gen_def!(identity, af_identity);
 
+/// Create a diagonal matrix
+///
+/// # Parameters
+///
+/// - `input` is the input Array
+/// - `dim` is the diagonal index relative to principal diagonal where values from input Array are
+/// to be placed
+///
+/// # Return Values
+///
+/// An Array with values as a diagonal Matrix
 #[allow(unused_mut)]
 pub fn diag_create(input: &Array, dim: i32) -> Result<Array, AfError> {
     unsafe {
@@ -263,6 +312,16 @@ pub fn diag_create(input: &Array, dim: i32) -> Result<Array, AfError> {
     }
 }
 
+/// Extract diagonal from a given Matrix
+///
+/// # Parameters
+///
+/// - `input` is the input Matrix
+/// - `dim` is the index of the diagonal that has to be extracted from the input Matrix
+///
+/// # Return Values
+///
+/// An Array with values of the diagonal from input Array
 #[allow(unused_mut)]
 pub fn diag_extract(input: &Array, dim: i32) -> Result<Array, AfError> {
     unsafe {
@@ -276,6 +335,17 @@ pub fn diag_extract(input: &Array, dim: i32) -> Result<Array, AfError> {
     }
 }
 
+/// Join two arrays
+///
+/// # Parameters
+///
+/// - `dim` is the dimension along which the concatenation has to be done
+/// - `first` is the Array that comes first in the concatenation
+/// - `second` is the Array that comes last in the concatenation
+///
+/// # Return Values
+///
+/// Concatenated Array
 #[allow(unused_mut)]
 pub fn join(dim: i32, first: &Array, second: &Array) -> Result<Array, AfError> {
     unsafe {
@@ -289,6 +359,16 @@ pub fn join(dim: i32, first: &Array, second: &Array) -> Result<Array, AfError> {
     }
 }
 
+/// Join multiple arrays
+///
+/// # Parameters
+///
+/// - `dim` is the dimension along which the concatenation has to be done
+/// - `inputs` is the vector of Arrays that has to be concatenated
+///
+/// # Return Values
+///
+/// Concatenated Array
 #[allow(unused_mut)]
 pub fn join_many(dim: i32, inputs: Vec<&Array>) -> Result<Array, AfError> {
     unsafe {
@@ -302,6 +382,25 @@ pub fn join_many(dim: i32, inputs: Vec<&Array>) -> Result<Array, AfError> {
     }
 }
 
+/// Join multiple Arrays along a given dimension
+///
+/// # Examples
+///
+/// ```
+/// # #[macro_use] extern crate arrayfire;
+/// # fn main() {
+/// let a = randu(Dim4::new(&[5, 3, 1, 1]), Aftype::F32).unwrap();
+/// let b = randu(Dim4::new(&[5, 3, 1, 1]), Aftype::F32).unwrap();
+/// let c = randu(Dim4::new(&[5, 3, 1, 1]), Aftype::F32).unwrap();
+/// let d = join_many![2, a, b, c];
+/// # }
+/// ```
+///
+/// # Panics
+///
+/// Using this macro to create an Array from multiple Arrays doesn't implicitly check for errors
+/// during FFI calls. Therefore, make sure your inputs are correct. In case, you do need proper
+/// error checking, use the [function version](./fn.join_many.html) of this macro.
 // Using macro to implement join many wrapper
 #[macro_export]
 macro_rules! join_many {
@@ -344,6 +443,15 @@ data_func_def!(tile, af_tile);
 data_func_def!(reorder, af_reorder);
 data_func_def!(shift, af_shift);
 
+/// Change the shape of the Array
+///
+/// # Parameters
+///
+/// - `input` is the input Array
+/// - `dims` is the new dimensions to which the input Array is reshaped to
+///
+/// # Return Values
+/// Reshaped Array
 #[allow(unused_mut)]
 pub fn moddims(input: &Array, dims: Dim4) -> Result<Array, AfError> {
     unsafe {
@@ -357,6 +465,7 @@ pub fn moddims(input: &Array, dims: Dim4) -> Result<Array, AfError> {
     }
 }
 
+/// Flatten the multidimensional Array to an 1D Array
 #[allow(unused_mut)]
 pub fn flat(input: &Array) -> Result<Array, AfError> {
     unsafe {
@@ -369,6 +478,16 @@ pub fn flat(input: &Array) -> Result<Array, AfError> {
     }
 }
 
+/// Flip the Array
+///
+/// # Parameters
+///
+/// - `input` is the Array to be flipped
+/// - `dim` is the dimension along which the flip has to happen
+///
+/// # Return Values
+///
+/// Flipped Array
 #[allow(unused_mut)]
 pub fn flip(input: &Array, dim: u32) -> Result<Array, AfError> {
     unsafe {
@@ -381,6 +500,15 @@ pub fn flip(input: &Array, dim: u32) -> Result<Array, AfError> {
     }
 }
 
+/// Create lower triangular matrix
+///
+/// # Parameters
+///
+/// - `input` is the input Array
+/// - `is_unit_diag` dictates if the output Array should have 1's along diagonal
+///
+/// # Return Values
+/// Array
 #[allow(unused_mut)]
 pub fn lower(input: &Array, is_unit_diag: bool) -> Result<Array, AfError> {
     unsafe {
@@ -394,6 +522,15 @@ pub fn lower(input: &Array, is_unit_diag: bool) -> Result<Array, AfError> {
     }
 }
 
+/// Create upper triangular matrix
+///
+/// # Parameters
+///
+/// - `input` is the input Array
+/// - `is_unit_diag` dictates if the output Array should have 1's along diagonal
+///
+/// # Return Values
+/// Array
 #[allow(unused_mut)]
 pub fn upper(input: &Array, is_unit_diag: bool) -> Result<Array, AfError> {
     unsafe {
--- src/image/mod.rs
+++ src/image/mod.rs
@@ -74,6 +74,21 @@ extern {
                       tospace: uint8_t, fromspace: uint8_t) -> c_int;
 }
 
+/// Calculate the gradients
+///
+/// The gradients along the first and second dimensions are calculated simultaneously.
+///
+/// # Parameters
+///
+/// - `input` is the input Array
+///
+/// # Return Values
+///
+/// A tuple of Arrays.
+///
+/// The first Array is `dx` which is the gradient along the 1st dimension.
+///
+/// The second Array is `dy` which is the gradient along the 2nd dimension.
 #[allow(unused_mut)]
 pub fn gradient(input: &Array) -> Result<(Array, Array), AfError> {
     unsafe {
@@ -88,6 +103,16 @@ pub fn gradient(input: &Array) -> Result<(Array, Array), AfError> {
     }
 }
 
+/// Load Image into Array
+///
+/// # Parameters
+///
+/// - `filename` is aboslute path of the image to be loaded.
+/// - `is_color` indicates if the image file at given path is color or gray scale.
+///
+/// # Return Arrays
+///
+/// An Array with pixel values loaded from the image
 #[allow(unused_mut)]
 pub fn load_image(filename: String, is_color: bool) -> Result<Array, AfError> {
     unsafe {
@@ -102,6 +127,12 @@ pub fn load_image(filename: String, is_color: bool) -> Result<Array, AfError> {
     }
 }
 
+/// Save an Array to an image file
+///
+/// # Parameters
+///
+/// - `filename` is the abolute path(includes filename) at which input Array is going to be saved
+/// - `input` is the Array to be stored into the image file
 #[allow(unused_mut)]
 pub fn save_image(filename: String, input: &Array) -> Result<(), AfError> {
     unsafe {
@@ -114,6 +145,26 @@ pub fn save_image(filename: String, input: &Array) -> Result<(), AfError> {
     }
 }
 
+/// Resize an Image
+///
+/// Resizing an input image can be done using either NEAREST or BILINEAR interpolations.
+/// Nearest interpolation will pick the nearest value to the location, whereas bilinear
+/// interpolation will do a weighted interpolation for calculate the new size.
+///
+/// This function does not differentiate between images and data. As long as the array is defined
+/// and the output dimensions are not 0, it will resize any type or size of array.
+///
+/// # Parameters
+///
+/// - `input` is the image to be resized
+/// - `odim0` is the output height
+/// - `odim1` is the output width
+/// - `method` indicates which interpolation method to use for resizing. It uses enum
+/// [InterpType](./enum.InterpType.html) to identify the interpolation method.
+///
+/// # Return Values
+///
+/// Resized Array
 #[allow(unused_mut)]
 pub fn resize(input: &Array, odim0: i64, odim1: i64,
               method: InterpType) -> Result<Array, AfError> {
@@ -128,6 +179,35 @@ pub fn resize(input: &Array, odim0: i64, odim1: i64,
     }
 }
 
+/// Transform(Affine) an Image
+///
+/// The transform function uses an affine transform matrix to tranform an input image into a new
+/// one. The transform matrix tf is a 3x2 matrix of type float. The matrix operation is applied to each
+/// location (x, y) that is then transformed to (x', y') of the new array. Hence the transformation
+/// is an element-wise operation.
+///
+/// The operation is as below: tf = [r00 r10 r01 r11 t0 t1]
+///
+/// x' = x * r00 + y * r01 + t0; y' = x * r10 + y * r11 + t1;
+///
+/// Interpolation types of NEAREST, LINEAR, BILINEAR and CUBIC are allowed. Affine transforms can be used for various purposes. [translate](./fn.translate.html), [scale](./fn.scale.html) and [skew](./fn.skew.html) are
+/// specializations of the transform function.
+///
+/// This function can also handle batch operations.
+///
+/// # Parameters
+///
+/// - `input` is the image to be resized
+/// - `trans` is the transformation matrix to be used for image transformation
+/// - `odim0` is the output height
+/// - `odim1` is the output width
+/// - `method` indicates which interpolation method to use for resizing. It uses enum
+/// [InterpType](./enum.InterpType.html) to identify the interpolation method.
+/// - `is_inverse` indicates if to apply inverse/forward transform
+///
+/// # Return Values
+///
+/// Resized Array
 #[allow(unused_mut)]
 pub fn transform(input: &Array, trans: &Array, odim0: i64, odim1: i64,
                  method: InterpType, is_inverse: bool) -> Result<Array, AfError> {
@@ -144,6 +224,35 @@ pub fn transform(input: &Array, trans: &Array, odim0: i64, odim1: i64,
     }
 }
 
+/// Rotate an Image
+///
+/// Rotating an input image can be done using either NEAREST or BILINEAR interpolations.
+/// Nearest interpolation will pick the nearest value to the location, whereas bilinear
+/// interpolation will do a weighted interpolation for calculate the new size.
+///
+/// This function does not differentiate between images and data. As long as the array is defined,
+/// it will rotate any type or size of array.
+///
+/// The crop option allows you to choose whether to resize the image. If crop is set to false, ie.
+/// the entire rotated image will be a part of the array and the new array size will be greater
+/// than or equal to the input array size. If crop is set to true, then the new array size is same
+/// as the input array size and the data that falls outside the boundaries of the array is
+/// discarded.
+///
+/// Any location of the rotated array that does not map to a location of the input array is set to
+/// 0.
+///
+/// # Parameters
+///
+/// - `input` is the input image
+/// - `theta` is the amount of angle (in radians) image should be rotated
+/// - `crop` indicates if the rotated image has to be cropped to original size
+/// - `method` indicates which interpolation method to use for rotating the image. It uses enum
+/// [InterpType](./enum.InterpType.html) to identify the interpolation method.
+///
+/// # Return Values
+///
+/// Rotated Array
 #[allow(unused_mut)]
 pub fn rotate(input: &Array, theta: f64, crop: bool,
               method: InterpType) -> Result<Array, AfError> {
@@ -183,6 +292,30 @@ macro_rules! trans_func_def {
 trans_func_def!(translate, af_translate);
 trans_func_def!(scale, af_scale);
 
+/// Skew an image
+///
+/// Skew function skews the input array along dim0 by skew0 and along dim1 by skew1. The skew
+/// areguments are in radians. Skewing the data means the data remains parallel along 1 dimensions
+/// but the other dimensions gets moved along based on the angle. If both skew0 and skew1 are
+/// specified, then the data will be skewed along both directions. Explicit output dimensions
+/// can be specified using odim0 and odim1. All new values that do not map to a location of the input array are set to 0.
+///
+/// Skew is a special case of the [transform](./fn.transform.html) function.
+///
+/// # Parameters
+///
+/// - `input` is the image to be skewed
+/// - `skew0` is the factor by which data is skewed along first dimension
+/// - `skew1` is the factor by which data is skewed along second dimension
+/// - `odim0` is the output length along first dimension
+/// - `odim1` is the output length along second dimension
+/// - `method` indicates which interpolation method to use for rotating the image. It uses enum
+/// [InterpType](./enum.InterpType.html) to identify the interpolation method.
+/// - `is_inverse` indicates if to apply inverse/forward transform
+///
+/// # Return Values
+///
+/// Skewed Image
 #[allow(unused_mut)]
 pub fn skew(input: &Array, skew0: f32, skew1: f32, odim0: i64, odim1: i64,
             method: InterpType, is_inverse: bool) -> Result<Array, AfError> {
@@ -199,6 +332,29 @@ pub fn skew(input: &Array, skew0: f32, skew1: f32, odim0: i64, odim1: i64,
     }
 }
 
+/// Compute Histogram of an Array
+///
+/// A histogram is a representation of the distribution of given data. This representation is
+/// essentially a graph consisting of the data range or domain on one axis and frequency of
+/// occurence on the other axis. All the data in the domain is counted in the appropriate bin. The
+/// total number of elements belonging to each bin is known as the bin's frequency.
+///
+/// The regular histogram function creates bins of equal size between the minimum and maximum of
+/// the input data (min and max are calculated internally). The histogram min-max function takes
+/// input parameters minimum and maximum, and divides the bins into equal sizes within the range
+/// specified by min and max parameters. All values less than min in the data range are placed in
+/// the first (min) bin and all values greater than max will be placed in the last (max) bin.
+///
+/// # Parameters
+///
+/// - `input` is the Array whose histogram has to be computed
+/// - `nbins` is the number bins the input data has to be categorized into.
+/// - `minval` is the minimum value of bin ordering
+/// - `maxval` is the maximum value of bin ordering
+///
+/// # Return Values
+///
+/// Histogram of input Array
 #[allow(unused_mut)]
 pub fn histogram(input: &Array, nbins: u32,
                  minval: f64, maxval: f64) -> Result<Array, AfError> {
@@ -235,6 +391,25 @@ morph_func_def!(erode, af_erode);
 morph_func_def!(dilate3, af_dilate3);
 morph_func_def!(erode3, af_erode3);
 
+/// Bilateral Filter.
+///
+/// A bilateral filter is a edge-preserving filter that reduces noise in an image. The intensity of
+/// each pixel is replaced by a weighted average of the intensities of nearby pixels. The weights
+/// follow a Gaussian distribution and depend on the distance as well as the color distance.
+///
+/// The bilateral filter requires the size of the filter (in pixels) and the upper bound on color
+/// values, N, where pixel values range from 0–N inclusively.
+///
+/// # Parameters
+///
+/// - `input` array is the input image
+/// - `spatial_sigma` is the spatial variance parameter that decides the filter window
+/// - `chromatic_sigma` is the chromatic variance parameter
+/// - `iscolor` indicates if the input is color image or grayscale
+///
+/// # Return Values
+///
+/// Filtered Image - Array
 #[allow(unused_mut)]
 pub fn bilateral(input: &Array, spatial_sigma: f32, chromatic_sigma: f32,
                  iscolor: bool) -> Result<Array, AfError> {
@@ -250,6 +425,26 @@ pub fn bilateral(input: &Array, spatial_sigma: f32, chromatic_sigma: f32,
     }
 }
 
+/// Meanshift Filter.
+///
+/// A meanshift filter is an edge-preserving smoothing filter commonly used in object tracking and
+/// image segmentation.
+///
+/// This filter replaces each pixel in the image with the mean of the values within a given given
+/// color and spatial radius. The meanshift filter is an iterative algorithm that continues until a
+/// maxium number of iterations is met or until the value of the means no longer changes.
+///
+/// # Parameters
+///
+/// - `input` array is the input image
+/// - `spatial_sigma` is the spatial variance parameter that decides the filter window
+/// - `chromatic_sigma` is the chromatic variance parameter
+/// - `iter` is the number of iterations filter operation is performed
+/// - `iscolor` indicates if the input is color image or grayscale
+///
+/// # Return Values
+///
+/// Filtered Image - Array
 #[allow(unused_mut)]
 pub fn mean_shift(input: &Array, spatial_sigma: f32, chromatic_sigma: f32,
                  iter: u32, iscolor: bool) -> Result<Array, AfError> {
@@ -287,6 +482,27 @@ filt_func_def!(medfilt, af_medfilt);
 filt_func_def!(minfilt, af_minfilt);
 filt_func_def!(maxfilt, af_maxfilt);
 
+/// Creates a Gaussian Kernel.
+///
+/// This function creates a kernel of a specified size that contains a Gaussian distribution. This
+/// distribution is normalized to one. This is most commonly used when performing a Gaussian blur
+/// on an image. The function takes two sets of arguments, the size of the kernel (width and height
+/// in pixels) and the sigma parameters (for row and column) which effect the distribution of the
+/// weights in the y and x directions, respectively.
+///
+/// Changing sigma causes the weights in each direction to vary. Sigma is calculated internally as
+/// (0.25 * rows + 0.75) for rows and similarly for columns.
+///
+/// # Parameters
+///
+/// - `rows` is number of rows of kernel
+/// - `cols` is number of cols of kernel
+/// - `sigma_r` is standard deviation of rows
+/// - `sigma_c` is standard deviation of cols
+///
+/// # Return Values
+///
+/// An Array with gaussian kernel values
 #[allow(unused_mut)]
 pub fn gaussian_kernel(rows: i32, cols: i32,
                        sigma_r: f64, sigma_c: f64) -> Result<Array, AfError> {
@@ -302,6 +518,38 @@ pub fn gaussian_kernel(rows: i32, cols: i32,
     }
 }
 
+/// Color space conversion
+///
+/// Following are the supported conversions
+///
+/// - RGB => GRAY
+/// - GRAY => RGB
+/// - RGB => HSV
+/// - HSV => RGB
+///
+/// RGB (Red, Green, Blue) is the most common format used in computer imaging. RGB stores
+/// individual values for red, green and blue, and hence the 3 values per pixel. A combination of
+/// these three values produces the gamut of unique colors.
+///
+/// HSV (Hue, Saturation, Value), also known as HSB (hue, saturation, brightness), is often used by
+/// artists because it is more natural to think about a color in terms of hue and saturation than
+/// in terms of additive or subtractive color components (as in RGB). HSV is a transformation of
+/// RGB colorspace; its components and colorimetry are relative to the RGB colorspace from which it
+/// was derived. Like RGB, HSV also uses 3 values per pixel.
+///
+/// GRAY is a single channel color space where pixel value ranges from 0 to 1. Zero represents
+/// black, one represent white and any value between zero & one is a gray value
+///
+/// # Parameters
+///
+/// - `input` is the input image
+/// - `tospace` is the target color space. Takes values of [ColorSpace](./enum.ColorSpace.html)
+/// - `fromspace` is the source image color space. Takes values of
+/// [ColorSpace](./enum.ColorSpace.html)
+///
+/// # Return Values
+///
+/// An Array with input image values in target color space
 #[allow(unused_mut)]
 pub fn color_space(input: &Array,
                    tospace: ColorSpace, fromspace: ColorSpace) -> Result<Array, AfError> {
@@ -316,6 +564,24 @@ pub fn color_space(input: &Array,
     }
 }
 
+/// Find blobs in given image.
+///
+/// Given a binary image (with zero representing background pixels), regions computes a floating
+/// point image where each connected component is labeled from 1 to N, the total number of
+/// components in the image.
+///
+/// A component is defined as one or more nonzero pixels that are connected by the specified
+/// connectivity (either [`Connectivity::FOUR`](./enum.Connectivity.html) or [`Connectivity::EIGHT`](./enum.Connectivity.html)) in two dimensions.
+///
+/// # Parameters
+///
+/// - `input` is the input image
+/// - `conn` can take one of the values of [Connectivity](./enum.Connectivity.html)
+/// - `aftype` can take one of the values of [Aftype](./enum.Aftype.html)
+///
+/// # Return Values
+///
+/// Array with labels indicating different regions
 #[allow(unused_mut)]
 pub fn regions(input: &Array, conn: Connectivity, aftype: Aftype) -> Result<Array, AfError> {
     unsafe {
@@ -329,6 +595,23 @@ pub fn regions(input: &Array, conn: Connectivity, aftype: Aftype) -> Result<Arra
     }
 }
 
+/// Sobel Operator
+///
+/// Sobel operators perform a 2-D spatial gradient measurement on an image to emphasize the regions
+/// of high spatial frequency, namely edges. A more in depth discussion on it can be found [here](https://en.wikipedia.org/wiki/Sobel_operator).
+///
+/// # Parameters
+///
+/// - `input` is the input image
+/// - `ker_size` is the kernel size of sobel operator
+///
+/// # Return Values
+///
+/// A tuple of Arrays.
+///
+/// The first Array has derivatives along horizontal direction
+///
+/// The second Array has derivatives along vertical direction
 #[allow(unused_mut)]
 pub fn sobel(input: &Array, ker_size: u32) -> Result<(Array, Array), AfError> {
     unsafe {
@@ -343,6 +626,15 @@ pub fn sobel(input: &Array, ker_size: u32) -> Result<(Array, Array), AfError> {
     }
 }
 
+/// Histogram Equalization
+///
+/// # Parameters
+///
+/// - `input` is the input Array to be equalized
+/// - `hist` is the Array to be used for equalizing input
+///
+/// # Return Values
+/// Equalized Array
 #[allow(unused_mut)]
 pub fn hist_equal(input: &Array, hist: &Array) -> Result<Array, AfError> {
     unsafe {
@@ -358,6 +650,13 @@ pub fn hist_equal(input: &Array, hist: &Array) -> Result<Array, AfError> {
 
 macro_rules! grayrgb_func_def {
     ($fn_name: ident, $ffi_name: ident) => (
+        /// Color space conversion functions
+        ///
+        /// # Parameters
+        ///
+        /// - `r` is fraction of red channel to appear in output
+        /// - `g` is fraction of green channel to appear in output
+        /// - `b` is fraction of blue channel to appear in output
         #[allow(unused_mut)]
         pub fn $fn_name(input: &Array, r: f32, g: f32, b: f32) -> Result<Array, AfError> {
             unsafe {
@@ -378,6 +677,7 @@ grayrgb_func_def!(gray2rgb, af_gray2rgb);
 
 macro_rules! hsvrgb_func_def {
     ($fn_name: ident, $ffi_name: ident) => (
+        /// Color space conversion functions
         #[allow(unused_mut)]
         pub fn $fn_name(input: &Array) -> Result<Array, AfError> {
             unsafe {
