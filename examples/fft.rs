85f2a900039d92402f8fedc9df94054fa1fd15a7
pradeep <pradeep@arrayfire.com>
2019-05-22 11:06:33 +0530

    Simple fft on complex signal example

---
+++ examples/fft.rs
@@ -0,0 +1,35 @@
+use arrayfire::*;
+use num::Complex;
+
+type Complex32 = Complex<f32>;
+
+#[allow(unused_must_use)]
+#[allow(unused_variables)]
+fn main() {
+    set_device(0);
+    info();
+    let samples = 10;
+    let dims = Dim4::new(&[samples, 1, 1, 1]);
+
+    let values = vec![
+        Complex::new(0.0, 2.0),
+        Complex::new(0.0, 2.0),
+        Complex::new(0.0, 2.0),
+        Complex::new(0.0, 2.0),
+        Complex::new(0.0, 2.0),
+        Complex::new(0.0, 2.0),
+        Complex::new(0.0, 2.0),
+        Complex::new(0.0, 2.0),
+        Complex::new(0.0, 2.0),
+        Complex::new(0.0, 2.0)
+    ];
+
+    let signal = Array::new(&values, dims);
+
+    af_print!("signal", signal);
+
+    // Used length of input signal as norm_factor
+    let output = fft(&signal, 0.1, samples as i64);
+
+    af_print!("Output", output);
+}
