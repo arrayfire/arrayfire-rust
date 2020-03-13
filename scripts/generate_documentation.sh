7e473dfe55427b509072ade6bb1397e3e8cdca50
pradeep <pradeep@arrayfire.com>
2020-03-13 12:56:51 +0530

    Avoid generating dependency crates documentation

--- scripts/generate_documentation.sh
+++ scripts/generate_documentation.sh
@@ -2,6 +2,6 @@
 
 # this script meant to be run from the root of arrayfire-rust
 
-cargo rustdoc -- --html-in-header ./scripts/mathjax.script
+cargo rustdoc -p arrayfire -- --html-in-header ./scripts/mathjax.script
 
 mdbook build tutorials-book && cp -r tutorials-book/book ./target/doc/arrayfire/
