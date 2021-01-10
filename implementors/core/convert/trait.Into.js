de7e07db7b72d3042f6c203fd234cde9bdf95552
pradeep <pradeep@arrayfire.com>
2021-01-10 12:54:43 +0530

    publish: Fix Crate version to 3.8 in README instructions
    generated from commit 5110e9814e3be5fdf69d20ae501736251711ea6e

--- implementors/core/convert/trait.Into.js
+++ implementors/core/convert/trait.Into.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["arrayfire"] = [{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"arrayfire/trait.HasAfEnum.html\" title=\"trait arrayfire::HasAfEnum\">HasAfEnum</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"arrayfire/struct.Array.html\" title=\"struct arrayfire::Array\">Array</a>&lt;T&gt;&gt; for <a class=\"type\" href=\"arrayfire/type.af_array.html\" title=\"type arrayfire::af_array\">af_array</a>","synthetic":false,"types":["arrayfire::core::util::af_array"]}];
+implementors["arrayfire"] = [{"text":"impl&lt;T:&nbsp;HasAfEnum&gt; Into&lt;Array&lt;T&gt;&gt; for af_array","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
